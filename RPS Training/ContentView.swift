b52c67e55dfae3f643f1db09fe11ed8ff3812f2d
Hugo Anjos <hugo.h.anjos@hotmail.com>
2020-11-18 19:31:27 -0300

    Initial Commit

---
+++ RPS Training/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RPS Training
+//
+//  Created by Hugo Anjos on 18/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RPS Training/RPS_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_TrainingApp.swift
+//  RPS Training
+//
+//  Created by Hugo Anjos on 18/11/20.
+//
+
+import SwiftUI
+
+@main
+struct RPS_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
