01cd5e795dc40140100372d06ace896e317dce61
Hugo Anjos <hugo.h.anjos@hotmail.com>
2020-11-18 20:28:51 -0300

    template

--- RPS Training/ContentView.swift
+++ RPS Training/ContentView.swift
@@ -8,9 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentMove = Int.random(in: 0...2)
+    @State private var winCondition = Bool.random()
+     
+    private var moves = ["Rock", "Paper", "Scissors"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black.ignoresSafeArea(.all)
+            VStack(spacing: 150) {
+                
+                VStack(spacing: 50) {
+                    Text("Player Score: 0")
+                        .foregroundColor(.white)
+                        .font(.title)
+                    
+                    VStack {
+                        Text("The move is:")
+                            .foregroundColor(.white)
+                            .font(.title)
+                        Text(moves[currentMove])
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                            .fontWeight(.black)
+                    }
+                    
+                    if winCondition {
+                        HStack {
+                            Text("You need to")
+                                .foregroundColor(.white)
+                                .font(.largeTitle)
+                            Text("WIN")
+                                .foregroundColor(.green)
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                        }
+                    } else {
+                        HStack {
+                            Text("You need to")
+                                .foregroundColor(.white)
+                                .font(.largeTitle)
+                            Text("LOSE")
+                                .foregroundColor(.red)
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                        }
+                    }
+                }
+                
+                HStack(spacing:10) {
+                    ForEach(0 ..< 3) { number in
+                        Button(action: {
+                            // botão
+                        }) {
+                            Text(self.moves[number])
+                                .foregroundColor(.white)
+                                .font(.largeTitle)
+                            
+                        }
+                    }
+                }
+                
+            }
+        }
     }
 }
 
